31d68be3f418445220c9782e369213ab32cb8d80
john bruyere <john.bruyere@orange.fr>
2016-01-05 12:28:37 +0100

    Creation de la fenêtre d'accueil

--- Surveillance_de_Sites/fen_accueil.cpp
+++ Surveillance_de_Sites/fen_accueil.cpp
@@ -1,9 +1,7 @@
 #include "fen_accueil.h"
 #include "ui_fen_accueil.h"
 
-FEN_Accueil::FEN_Accueil(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::FEN_Accueil)
+FEN_Accueil::FEN_Accueil(QWidget *parent) : QMainWindow(parent), ui(new Ui::FEN_Accueil)
 {
     ui->setupUi(this);
 }
--- Surveillance_de_Sites/main.cpp
+++ Surveillance_de_Sites/main.cpp
@@ -4,8 +4,11 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    FEN_Accueil w;
-    w.show();
+
+    //Création de la fenêtre d'acceuil
+    FEN_Accueil fenAccueil;//Création de l'instance FEN_Acceuil
+    fenAccueil.setFixedSize(403, 187);//Exiger une taille fixe de 403px en largeur et 187px en hauteur
+    fenAccueil.show();//Affichage de la fenêtre d'accueil
 
     return a.exec();
 }
