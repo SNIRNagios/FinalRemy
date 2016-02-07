d1632e08a6e2950e30fd9727a52417e1f0000bd9
John Bruyere <john.bruyere@orange.fr>
2016-02-07 22:00:30 +0100

    Ajout de commentaire sur fen_superviseur - partie 1

--- Superviseur/QSuperviseur/fen_superviseur.cpp
+++ Superviseur/QSuperviseur/fen_superviseur.cpp
@@ -1,51 +1,74 @@
+/*
+fichier     fen_superviseur.cpp
+résumé      Programme destiné à échanger des données avec un collecteur distant de type Raspberry Pi (nagios incl.).
+            Ce programme est capable de traiter et d'analyser les données reçues sous forme de tableaux.
+auteur      John BRUYERE
+version     1.0
+date        07/02/2016
+*/
+
 #include "fen_superviseur.h"
 #include "ui_fen_superviseur.h"
 
-//Julien DESGOUTTE
-//Julien.DESGOUTTE@lesvallonsdelatour.fr
-
+/*
+résumé      Constructeur par défaut ayant pour objectif de connecter des signaux aux slots et de pouvoir désactiver/cacher
+            des éléments dés l'ouverture de la fenêtre.
+*/
 fen_superviseur::fen_superviseur(QWidget *parent) : QDialog(parent), ui(new Ui::fen_superviseur)
 {
     ui->setupUi(this);
+
+    //Allocation dynamique
     statusLabel = new QLabel(this);
     site = new Collecteur(this);
 
-    //CREATION DU TIMER
+    //Création du timer
     timer = new QTimer();
-
-    connect(timer, SIGNAL(timeout()), this, SLOT(interrogation()));
-    //FIN DU TIMER
 
     demande = 0;
     k = 0;
-
     portCollecteur = 6557;
-    portCollecteurStr = QString::number(portCollecteur);
+    portCollecteurStr = QString::number(portCollecteur);//convertir le port en QString
 
-    connect(site, SIGNAL(vers_IHM_texte(QString)),this,SLOT(obtenirSocket(QString)));
-    connect(ui->TW_Hotes, SIGNAL(cellClicked(int,int)),this,SLOT(analyseProblemeHote(int, int)));
+    //Connexion SIGNAUX-SLOTS
+    connect(timer, SIGNAL(timeout()), this, SLOT(interrogation()));//Le siganl timeout() et connecté au slot interrogation()
+    connect(site, SIGNAL(vers_IHM_texte(QString)),this,SLOT(obtenirSocket(QString)));//Le signal vers_IHM_texte(QString) est connecté au slot ObtenirSocket(QString)
+    connect(ui->TW_Hotes, SIGNAL(cellClicked(int,int)),this,SLOT(analyseProblemeHote(int, int)));//Le signal cellClicked(int,int)est connecté au slot analyseProblemeHote(int,int)
 
-    ui->TW_Hotes->setEditTriggers(QAbstractItemView::EditTriggers(0));
+    //AUTRES
+    ui->TW_Hotes->setEditTriggers(QAbstractItemView::EditTriggers(0));//On rend impossible l'édition des cellules de TW_Hotes
 
+    //Appels de fonctions
+    initialisationTableHote();//Appel de fonction pour initialiser TW_Hotes
+    initialisationTableService();//Appel de fonction pour initialiser TW_Services
 
-
-    initialisationTableHote();
-    initialisationTableService();
-
+    //Désactivation provisoire
     ui->GB_Contenu->setEnabled(false);
     ui->BTN_Deconnexion->setEnabled(false);
     ui->BTN_StopTimer->setEnabled(false);
 
+    //Element à cacher provisoirement
     ui->BTN_getHosts->hide();
     ui->BTN_getServices->hide();
 }
 
+/*
+résumé      Destructeur.
+*/
 fen_superviseur::~fen_superviseur()
 {
     delete ui;
     delete statusLabel;
+    delete site;
+    delete timer;
 }
 
+/*
+résumé      Ce slot est activé lorsque qu'un utilisateur clique sur "Obtenir les Hôtes".
+            Il se connecte à un collecteur pour pouvoir lui envoyer une requête LQL
+param       Aucun
+return      Aucun
+*/
 void fen_superviseur::on_BTN_getHosts_clicked()
 {
     adresseCollecteur = ui->comboBox->currentText();
@@ -55,6 +78,12 @@ void fen_superviseur::on_BTN_getHosts_clicked()
     ui->BTN_getHosts->setEnabled(false);
 }
 
+/*
+résumé      Ce slot est activé lorsque qu'un utilisateur clique sur "Obtenir les Services".
+            Il se connecte à un collecteur pour pouvoir lui envoyer une requête LQL
+param       Aucun
+return      Aucun
+*/
 void fen_superviseur::on_BTN_getServices_clicked()
 {
     adresseCollecteur = ui->comboBox->currentText();
@@ -64,6 +93,13 @@ void fen_superviseur::on_BTN_getServices_clicked()
     ui->BTN_getServices->setEnabled(false);
 }
 
+/*
+résumé      Ce slot est activé lorsque le signal "vers_IHM_texte" de collecteur.cpp est émis.
+            Ce slot place le contenu du socket obtenu dans un QString nommé "contenu" et fait appel aux fonctions
+            "traitement()" et "insertion()" avant de retourner "contenu".
+param       socketLivestatus [QString]
+return      contenu [QString]
+*/
 QString fen_superviseur::obtenirSocket(QString socketLivestatus)
 {
     contenu = socketLivestatus;
@@ -72,6 +108,12 @@ QString fen_superviseur::obtenirSocket(QString socketLivestatus)
     return contenu;
 }
 
+/*
+résumé      Cette méthode publique traite la variable contenu par le biais d'un QRegExp (expression) qui coupe la chaîne de caractére
+            dés que celle-ci rencontre les caractéres ";" et "\n".Une fois coupé, ces QString sont envoyées dans une QStringList nommé "liste"
+param       Aucun
+return      Aucun
+*/
 void fen_superviseur::traitement()
 {
     //Création de l'expression ";" et "\n" afin de pouvoir splitter le contenu de la socket avec ces caractéres
@@ -81,10 +123,16 @@ void fen_superviseur::traitement()
     liste = contenu.split(caractere);
 }
 
+/*
+résumé      Cette méthode publique affiche les hôtes dans le QTextEdit sous forme de liste.
+            Elle en profite également pour associer le nom d'hôte et son état fonctionnel dans une QMap nommé "equipements"
+param       Aucun
+return      Aucun
+*/
 void fen_superviseur::afficherHote()
 {
-    //Inserer et associer le nom et l'etat de l'équipement dans le QMAP
-    for (int i = 0; i < liste.size()-1; i++)
+
+    for (int i = 0; i < liste.size()-1; i++)//Inserer et associer le nom et l'etat de l'équipement dans le QMAP
     {
        equipements[liste[i]] = liste[i+1];
        i++;
@@ -102,10 +150,18 @@ void fen_superviseur::afficherHote()
     }
     else
     {
-        ui->TE_Hotes->append("No devices");
+        ui->TE_Hotes->append("Aucun équipements");
     }
 }
 
+
+/*
+résumé      Cette méthode publique insert les QStrings de la QStringList dans une QTableWidget en fonction de la demande envoyée.
+            - Si la demande est de 1, alors on insert les éléments concernant les hôtes dans TW_Hôtes
+            - Si la demande est de 2, alors on insert les éléments concernant les services dans TW_Services
+param       Aucun
+return      Aucun
+*/
 void fen_superviseur::insertion()
 {
 
@@ -224,17 +280,27 @@ void fen_superviseur::insertion()
     }
 }
 
+/*
+résumé      Ce slot est activé lorsqu'un utilisateur clique sur le bouton "Connexion".
+            Il désactive et active certains éléments et fait apparaître deux nouveaux boutons nommé "BTN_getHosts" et "BTN_getServices".
+            Sa fonction principale est de se connecter à un collecteur par le biais d'une instance "site" (collecteur rapsberry pi)
+param       Aucun
+return      Aucun
+*/
 void fen_superviseur::on_BTN_Connexion_clicked()
 {
+    //Activation provisoire
     ui->BTN_Deconnexion->setEnabled(true);
-    ui->GB_Contenu->setEnabled(true);
-    ui->BTN_Connexion->setEnabled(false);
+    ui->GB_Contenu->setEnabled(true);  
     ui->BTN_getHosts->setEnabled(true);
     ui->BTN_getServices->setEnabled(true);
+
+    //Désactivation provisoire
     ui->comboBox->setEnabled(false);
     ui->GB_Interrogation->setEnabled(false);
+    ui->BTN_Connexion->setEnabled(false);
 
-
+    //Rend visible
     ui->BTN_getHosts->show();
     ui->BTN_getServices->show();
 
@@ -242,6 +308,13 @@ void fen_superviseur::on_BTN_Connexion_clicked()
     ui->GB_Contenu->setTitle(adresseCollecteur + ":" + portCollecteurStr);
 }
 
+/*
+résumé      Ce slot est activé lorsqu'un utilisateur clique sur le bouton "Déconnexion".
+            Il désactive et active certains éléments et cache les deux boutons nommé "BTN_getHosts" et "BTN_getServices".
+            Sa fonction principale est de se déconnecter d'un collecteur par le biais d'une instance "site" (collecteur rapsberry pi)
+param       Aucun
+return      Aucun
+*/
 void fen_superviseur::on_BTN_Deconnexion_clicked()
 {
     SupressionHote();
@@ -279,56 +352,57 @@ void fen_superviseur::SupressionHote()
 
     ui->TE_Hotes->clear();
 
-    initialisationTableHote();
+    initialisationTableHote();//Appel de la fonction initialisationTableService
 }
 
 void fen_superviseur::SupressionService()
 {
-    ui->TW_Services->setColumnHidden(2, false);
+    ui->TW_Services->setColumnHidden(2, false);//On rend visible la troisiéme colonne de TW_Service afin de pouvoir supprimer sont contenu
 
-    int nbColonneService = ui->TW_Services->columnCount();
+    int nbColonneService = ui->TW_Services->columnCount();//nbColonneService prend le nombre total de colonne crée dans TW_Services
     int nbLigneService = ui->TW_Services->rowCount();
 
 
-    ui->TW_Services->clearContents();
+    ui->TW_Services->clearContents();//On néttoie le contenu de TW_Services
 
-    for (int h = 0; h < nbColonneService; h++)
+    for (int h = 0; h < nbColonneService; h++)//On parcours les colonnes de TW_Hotes
     {
-        ui->TW_Services->removeColumn(h);
-        for (int j = 0; j < nbLigneService; j++)
+        ui->TW_Services->removeColumn(h);//On supprime toutes les colonnes parcourues
+        for (int j = 0; j < nbLigneService; j++)//On parcours les lignes de TW_Services
         {
-            ui->TW_Services->removeRow(j);
+            ui->TW_Services->removeRow(j);//On supprime toutes les lignes parcourues
         }
     }
 
-    initialisationTableService();
+    initialisationTableService();//Appel de la fonction initialisationTableService
 }
 
 void fen_superviseur::initialisationTableHote()
 {
-    //TABLE WIDGET HOTES/////////////////////////////////////////////////////////////
-    ui->TW_Hotes->setColumnCount(3);
-    ui->TW_Hotes->horizontalHeader()->setSectionResizeMode(0, QHeaderView::Stretch);
-    ui->TW_Hotes->horizontalHeader()->setSectionResizeMode(2, QHeaderView::Stretch);
-    QStringList enteteHote;
-    enteteHote << "Equipement" << "Etat" << "Statut";
-    ui->TW_Hotes->setHorizontalHeaderLabels(enteteHote);
-    ui->TW_Hotes->setColumnHidden(1, true);
-    //FIN DE LA TABLE HOTES//////////////////////////////////////////////////////////
+    ui->TW_Hotes->setColumnCount(3);//On ajoute 4 colonne à TW_Hotes
+
+    ui->TW_Hotes->horizontalHeader()->setSectionResizeMode(0, QHeaderView::Stretch);//On ajuste son entête 0
+    ui->TW_Hotes->horizontalHeader()->setSectionResizeMode(2, QHeaderView::Stretch);//On ajuste son entête 2
+
+    QStringList enteteHote;//Créationd'une liste de string
+    enteteHote << "Equipement" << "Etat" << "Statut";//On insére des QStrings dans la liste de QString
+    ui->TW_Hotes->setHorizontalHeaderLabels(enteteHote);//On insére la liste dans l'entete de TW_Services
+    ui->TW_Hotes->setColumnHidden(1, true);//On cache la seconde colonne pour une raison ergonomique
 }
 
 void fen_superviseur::initialisationTableService()
 {
-    //TABLE WIDGET SERVICE/////////////////////////////////////////////////////////////
-    ui->TW_Services->setColumnCount(4);//AJOUT DE 4 COLONNES///////////////////////////
-    ui->TW_Services->horizontalHeader()->setSectionResizeMode(0, QHeaderView::Stretch);
-    ui->TW_Services->horizontalHeader()->setSectionResizeMode(1, QHeaderView::Stretch);
-    ui->TW_Services->horizontalHeader()->setSectionResizeMode(3, QHeaderView::Stretch);
-    QStringList enteteService;
-    enteteService << "Equipement" << "Service "<< "Etat" << "Statut";
-    ui->TW_Services->setHorizontalHeaderLabels(enteteService);
-    ui->TW_Services->setColumnHidden(2, true);
-    //FIN DE LA TABLE WIDGET SERVICES//////////////////////////////////////////////////
+    ui->TW_Services->setColumnCount(4);//On ajoute 4 colonne à TW_Services
+
+    ui->TW_Services->horizontalHeader()->setSectionResizeMode(0, QHeaderView::Stretch);//On ajuste son entête 0
+    ui->TW_Services->horizontalHeader()->setSectionResizeMode(1, QHeaderView::Stretch);//On ajuste son entête 1
+    ui->TW_Services->horizontalHeader()->setSectionResizeMode(3, QHeaderView::Stretch);//On ajuste son entête 3
+
+    QStringList enteteService;//Créationd'une liste de string
+    enteteService << "Equipement" << "Service "<< "Etat" << "Statut";//On insére des QStrings dans la liste de QString
+    ui->TW_Services->setHorizontalHeaderLabels(enteteService);//On insére la liste dans l'entete de TW_Services
+    ui->TW_Services->setColumnHidden(2, true);//On cache la troisiéme colonne pour une raison ergonomique
+
 }
 
 void fen_superviseur::chargerThemeSombre()
@@ -356,16 +430,16 @@ void fen_superviseur::on_toolButton_clicked()
 
 void fen_superviseur::analyseProblemeHote(int ligne, int colonne)
 {
-    QMessageBox alerte;
-    alerte.setWindowTitle("Information");
+    QMessageBox alerte;//Création d'une QMessageBox nommé alerte
+    alerte.setWindowTitle("Information");//On attribut un titre au QMessageBox
 
-    if (ui->TW_Hotes->item(ligne,colonne)->data(0).toString() == "NON-ACTIF")
+    if (ui->TW_Hotes->item(ligne,colonne)->data(0).toString() == "NON-ACTIF")//Si, lors du clique, le contenu de l'item vaut "NON-ACTIF" alors
     {
         alerte.setText("L'envoi de ping sur " + ui->TW_Hotes->item(ligne,colonne-2)->data(0).toString() + " a échoué.\n\n"
                        "• Vérifier que l'équipement est bien connecté.\n"
                        "• Vérifier que l'équipement est bien en état de marche.\n"
-                       "• Vérifier les services ci-dessous.");
-        alerte.exec();
+                       "• Vérifier les services ci-dessous.");//On place un message d'aide dans la QMessageBox alerte
+        alerte.exec();//On excute la QMessageBox
 
     }
 
@@ -374,14 +448,12 @@ void fen_superviseur::analyseProblemeHote(int ligne, int colonne)
 
 void fen_superviseur::interrogation()
 {
-
-
-    if (ui->comboBox->currentText().isEmpty())
+    if (ui->comboBox->currentText().isEmpty())//Si l'item du combobox est une chaine de caractére vide
     {
         indexCollecteur = 0;
-        ui->comboBox->setCurrentIndex(indexCollecteur);
-        nombreBoucle++;
-        ui->LA_Boucle->setText("Boucle : " + QString::number(nombreBoucle));
+        ui->comboBox->setCurrentIndex(indexCollecteur);//On dirige l'index du combobox à 0
+        nombreBoucle++;//Incrémentation de la variable nombreBoucle
+        ui->LA_Boucle->setText("Boucle : " + QString::number(nombreBoucle));//On affiche le nombre de boucles exécutées
     }
     else
     {
@@ -391,7 +463,7 @@ void fen_superviseur::interrogation()
             adresseCollecteur = ui->comboBox->currentText();//On récupére le texte de l'item courant de la combobox
             ui->LA_Status->setText("Connexion à " + adresseCollecteur);//Le label de statut affiche un message
             k++;//Incrémentation de la variable k
-            indexCollecteur++;
+            indexCollecteur++;//Incrémentation de l'indexCollecteur
 
 
         }
@@ -436,7 +508,7 @@ void fen_superviseur::on_BTN_StopTimer_clicked()
 {
     timer->stop();//On arrête le timer en cours de fonctionnement
     ui->LCDN_Chrono->display(0);//On initialise le timer avec la valeur zéro
-    ui->LA_Status->setText("");//Le label de status n'affiche rien
+    ui->LA_Status->setText("");//Le label de statut n'affiche rien
     k = 0;//On remet la variable k à 0 pour pouvoir re-activer le timer sans aucun probléme.
     ui->comboBox->setCurrentIndex(0);
 
